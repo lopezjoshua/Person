4189a47db0eba078513b16ce9164ee9742a6dc6d
coffee247 <stallingsjm@vcu.edu>
2019-01-26 16:53:28 -0500

    Make setLastModified private to class Person

--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -60,7 +60,7 @@ public abstract class Person implements Serializable{
         }
     }
 
-    public void setLastModified()
+    private void setLastModified()
     {
         this.lastModifiedDate = LocalDate.now();
         this.lastModifiedTime = LocalTime.now();
